b2dd4bf2047d2f3d325e326be4833dde44392134
MD Shadhin <65449833+md-shadhin-mia@users.noreply.github.com>
2023-09-29 14:26:09 +0600

    Update QuartzDemoApplication.java

--- src/main/java/com/example/quartzdemo/QuartzDemoApplication.java
+++ src/main/java/com/example/quartzdemo/QuartzDemoApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuartzDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuartzDemoApplication.class, args);
+		SpringApplication.run(QuartzDemoApplication.class, args);	//here app is running 
 	}
 }
